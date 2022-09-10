385e62df0f83987926f2eea596abc540b3fb3f07
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2022-09-10 00:00:18 -0300

    header

--- tainara/src/App.vue
+++ tainara/src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <TainaraHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TainaraHeader from "./components/TainaraHeader";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TainaraHeader,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css?family=Rajdhani&display=swap");
 </style>
---
+++ tainara/src/components/TainaraHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="container">
+      <nav class="navbar navbar-expand">
+        <a href="#" class="link navbar-brand mr-auto">
+          <img
+            src="./../assets/logo.jpg"
+            alt="Tainara"
+            class="img d-inline-block align-top"
+            id="logo"
+          />
+        </a>
+        <div class="links mr-2">
+          <a href="#"> </a>
+          <a href="#"> </a>
+          <a href="#"> </a>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
